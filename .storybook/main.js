0735f2047fb33f71ee9b8bb140a3ddd45c291d14
Adrià Ventura Herce <aventura@adasasistemas.onmicrosoft.com>
2023-05-25 13:48:08 +0200

    ADD: atomic design to storybook

---
+++ .storybook/main.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const config = {
+  core: {
+    builder: "webpack5",
+  },
+  stories: [
+    "../src/**/*.mdx",
+    "../src/components/**/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/components": path.resolve(__dirname, "../src/components"),
+    };
+    return config;
+  },
+};
+export default config;
